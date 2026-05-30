e96008025f8f6ae9d0dafcbebe58cc14654bd58a
Sharif <stayhalalhabibi002@gmail.com>
2026-05-30 19:17:23 +0530

    add new file

---
+++ 3.py
@@ -0,0 +1,33 @@
+str1 = "sam"
+print(str1)
+print(type(str1))
+
+a = "10"
+print(a)
+print(type(a))
+
+b = 20
+print(type(b))
+
+s = True
+print(type(s))
+
+v = False
+print(type(v))
+
+n = "False"
+print(n)
+print(type(n))
+
+print(a,b,s,v,n,)
+
+name = "sharifur rahman"
+age = 20
+marks = 82.876
+print(name , "is", age , "years old and got the", marks ,
+      "marks")
+print("%s is %d years old and got the %f marks" %(name,age,marks))
+
+print(type(name))
+print(type(age))
+print(type(marks))
